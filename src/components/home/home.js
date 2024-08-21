48876daff82876c5a64f488a2263b5dd82374199
AnthonyChanng <155197190+AnthonyChanng@users.noreply.github.com>
2024-08-21 02:28:22 -0400

    1.9-Anthony
    Updated the menu to look better as well as made home page look better with the description starting to be formed

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,14 +1,21 @@
 import "./home.scss";
-const Home = () => {
+import boys from "../../assets/images/boysfinal.jpg";
+import girls from "../../assets/images/girls.jpg";
+import Description from "../Description/description";
+const Background = () => {
   return (
     <section className="topsection home-section">
       <div className="container home-container">
-        <div className="flex home-flex">
-          <h1 className="h1"> Bridgewater-Raritan Highschool Volleyball</h1>
+        <div className="backgrounds">
+          <img className="boyBackground" src={boys} alt="boys" />
+          <img className="girlBackground" src={girls} alt="girls" />
+        </div>
+        <div className="description">
+          <Description />
         </div>
       </div>
     </section>
   );
 };
 
-export default Home;
+export default Background;
