6d4ebb94d59cbaf5209ca4b90e9fd5e917844f75
AnthonyChanng <155197190+AnthonyChanng@users.noreply.github.com>
2024-08-25 23:47:37 -0400

    2.2-Anthony
    Implemented carousel which can be used for both the girls and the boys pages. Also bad the boys page more readable.

---
+++ src/assets/data/boysData.js
@@ -0,0 +1,34 @@
+import boys1 from "../images/boysGallery/boys1.jpg";
+import boys2 from "../images/boysGallery/boys2.jpg";
+import boys3 from "../images/boysGallery/boys3.jpg";
+import boys4 from "../images/boysGallery/boys4.jpg";
+import boys5 from "../images/boysGallery/boys5.jpg";
+import boys6 from "../images/boysGallery/boys6.jpg";
+import boys7 from "../images/boysGallery/boys7.jpg";
+import boys8 from "../images/boysGallery/boys8.jpg";
+export const boysData = [
+  {
+    image: boys1,
+  },
+  {
+    image: boys2,
+  },
+  {
+    image: boys3,
+  },
+  {
+    image: boys4,
+  },
+  {
+    image: boys5,
+  },
+  {
+    image: boys6,
+  },
+  {
+    image: boys7,
+  },
+  {
+    image: boys8,
+  },
+];
