6ba027aa3fce1f784f1cca2080cc1732b05c3526
AnthonyChanng <155197190+AnthonyChanng@users.noreply.github.com>
2024-09-01 16:43:51 -0400

    2.8-Anthony
    Made both girls/boys pages look a lot better with addition of team roster coach and recent news needs to be made more responsive

---
+++ src/components/Boys/boys.js
@@ -0,0 +1,189 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import hudl from "../../assets/images/hudl.png";
+import { boysData } from "../../assets/data/boysData";
+import harry from "../../assets/images/boysTeam/harry.jpg";
+import cam from "../../assets/images/boysTeam/cam.jpg";
+import aaryan from "../../assets/images/boysTeam/aaryan.jpg";
+import corey from "../../assets/images/boysTeam/corey.jpg";
+import andrew from "../../assets/images/boysTeam/andrew.jpg";
+import vihaan from "../../assets/images/boysTeam/vihaan.jpg";
+import rom from "../../assets/images/coachRom.jpg";
+import boysNews1 from "../../assets/images/boysNews1.jpg";
+import boysNews2 from "../../assets/images/boysNews2.jpg";
+import "./boys.scss";
+import { Carousel } from "../Carousel/carousel";
+export default function Boys() {
+  return (
+    <>
+      <div className="topsection boys-section">
+        <div className="container boys-container">
+          <div className="flex boys-flex">
+            <div className="boys-first-bar">
+              <div className="imageGallery">
+                <Carousel images={boysData} />
+              </div>
+              <div className="boys-rightSide">
+                <div className="boys-description">
+                  The BRHS Boys Volleyball Team are back-to-back county champs
+                  as well as back-to-back North Group 4 champs. They have
+                  dominated these two tournaments these past two years and look
+                  to win for the third year in a row. Many players gained awards
+                  for their performance throughout the season and everyone had
+                  their fair share of time in the spotlight. Although there were
+                  a few keys pieces who graduated last year, a large part of
+                  this championship core has remained put. <br></br>
+                  <br></br>The determination and strength across the team is not
+                  to be underestimated. After losing the first set in the county
+                  finals versus Ridge, and being down early in the last two
+                  sets, it looked all but over for the Panthers. However, the
+                  passion from every single player on the court shined through.
+                  Play after play, the practice and knowledge that was drilled
+                  into them by their coach, Corey Romanak, led them to make the
+                  right decisions and the eventual victory. <br></br>
+                  <br></br>One of the most important parts of the team is the
+                  great team chemistry that they have. From the countless pasta
+                  parties and bus rides to games and tournaments, there is an
+                  amazing bond that is hard to replicate anywhere else. If you
+                  have the passion for volleyball and strive to be a part of a
+                  winning culture, join the team! Tryouts are in late March and
+                  be sure to join the Google Classroom for more information.
+                </div>
+                <div className="boys-awards">
+                  <h1>TEAM AWARDS:</h1>
+                  Harrison Fromberg: Skyland Conference Boys Volleyball Player
+                  of the Year, Skyland Conference First Team, All-State Third
+                  Team, and All-Group 4 First Team <br></br>Sean Marvuglio:
+                  All-Group 4 Second Team and Skyland Conference First Team{" "}
+                  <br></br>Matthew Dolly: Skyland Conference First Team{" "}
+                  <br></br>Trevor Sullivan: Skyland Conference First Team{" "}
+                  <br></br>Vihaan Pradham: Skyland Conference Second Team{" "}
+                  <br></br>Cameron Willaims: Skyland Conference Second Team
+                  <br></br>Sudarshan Ganapathy: Conference Second Team<br></br>
+                  Coach Corey Romanak: Skyland Conference Coach of the Year
+                </div>
+                <div className="socials-boys">
+                  <div className="boys-insta">
+                    <a
+                      className="instagram"
+                      href="https://www.instagram.com/brboysvolleyball/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <FontAwesomeIcon
+                        icon={faInstagram}
+                        color="white"
+                        className="icon-boys"
+                      />
+                    </a>
+                    <div className="boys-title">Boys Instagram</div>
+                  </div>
+                  <div className="hudl-boys">
+                    <div className="BRVBhudl">
+                      <a
+                        href="https://fan.hudl.com/usa/nj/bridgewater/organization/24379/bridgewater-raritan-high-school/team/324160/boys-varsity-volleyball"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {" "}
+                        <img className="boys-Hudl" src={hudl} alt="hudl" /> {""}
+                      </a>
+                      <div className="boys-title">Boys Hudl</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="boys-players-tab">
+              <div className="boys-players-title">
+                <h1>THE 2025 BOYS BRVB VARSITY TEAM</h1>
+              </div>
+              <div className="boys-roster">
+                <div className="boys-player">
+                  <img className="boys-player-img" src={harry} alt="" />
+                  Harrison Fromberg
+                </div>
+                <div className="boys-player">
+                  <img className="boys-player-img" src={vihaan} alt="" />
+                  Vihaan Pradham
+                </div>
+                <div className="boys-player">
+                  <img className="boys-player-img" src={cam} alt="" />
+                  Cameron Willaims
+                </div>
+                <div className="boys-player">
+                  <img className="boys-player-img" src={andrew} alt="" />
+                  Andrew Zagula
+                </div>
+                <div className="boys-player">
+                  <img className="boys-player-img" src={aaryan} alt="" />
+                  Aaryan Arvind
+                </div>
+                <div className="boys-player">
+                  <img className="boys-player-img" src={corey} alt="" />
+                  Corey Amermann
+                </div>
+              </div>
+              <h3>MORE PLAYERS TO BE DETERMINED</h3>
+            </div>
+            <div className="boys-third-bar">
+              <div className="boys-coach">
+                <div className="coach-title">
+                  <h1>HEAD COACH COREY ROMANAK</h1>
+                </div>
+                <img src={rom} alt="romanak" className="romanak" />
+                <div className="coach-description">
+                  Coach Corey Romanak has been the head coach of the
+                  Bridgewater-Raritan High School Boys Varsity Volleyball
+                  program for over 25 years and was also the head coach of the
+                  girls program before stepping down last year. Under his
+                  leadership, the boys team has won two state championships and
+                  multiple county tournaments. The excellent coaching ability he
+                  possesses can't be underestimated. His skill and bravado push
+                  his players to do their best on and off the court and the
+                  level of respect that he commands from them is something that
+                  only a few people can achieve.
+                  <br></br>
+                  <br></br>
+                  As a player on the first NCAA Men's Volleyball Division III
+                  National Championship team in 1997, Coach Romanak knows what
+                  it takes to win, as a player and a coach. The bond that he
+                  shares with each of the kids is amazing. Over the four years
+                  that they are in the program, there is a close relationship
+                  formed between player and coach that is very rare. All the
+                  players enjoy being under his tutelage and learning from him.
+                </div>
+              </div>
+              <div className="boys-recent-news">
+                <div className="news-title">
+                  <h1>RECENT NJ BOYS VOLLEYBALL NEWS</h1>
+                </div>
+                <div className="boys-news-item">
+                  <a
+                    href="https://www.nj.com/highschoolsports/2024/06/boys-volleyball-all-state-full-postseason-honors-2024.html"
+                    target="blank"
+                    rel="noreferrer"
+                  >
+                    <img className="boys-news-img" src={boysNews1} alt="" />
+                  </a>
+                  Boys Volleyball All-State & full postseason honors, 2024
+                </div>
+                <div className="boys-news-item">
+                  <a
+                    href="https://www.nj.com/highschoolsports/2024/06/scotch-plains-fanwoods-tim-ennis-is-the-boys-volleyball-player-of-the-year-2024.html"
+                    target="blank"
+                    rel="noreferrer"
+                  >
+                    <img className="boys-news-img" src={boysNews2} alt="" />
+                  </a>
+                  Scotch Plains-Fanwood's Tim Ennis is the Boys Volleyball
+                  Player of the Year, 2024
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/Girls/girls.js
@@ -0,0 +1,234 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import hudl from "../../assets/images/hudl.png";
+import { girlsData } from "../../assets/data/girlsData";
+import liel from "../../assets/images/girlsTeam/liel.jpg";
+import jada from "../../assets/images/girlsTeam/jada.jpg";
+import liya from "../../assets/images/girlsTeam/liya.jpg";
+import jahniah from "../../assets/images/girlsTeam/jahniah.jpg";
+import kylie from "../../assets/images/girlsTeam/kylie.jpg";
+import ella from "../../assets/images/girlsTeam/ella.jpg";
+import hannah from "../../assets/images/girlsTeam/hannah.jpg";
+import irene from "../../assets/images/girlsTeam/irene.jpg";
+import blank from "../../assets/images/girlsTeam/blankGirls.png";
+import josh from "../../assets/images/coachJosh.jpg";
+import girlsNews1 from "../../assets/images/girlsNews1.png";
+import girlsNews2 from "../../assets/images/girlsNews2.jpg";
+import "./girls.scss";
+import { Carousel } from "../Carousel/carousel";
+export default function Girls() {
+  return (
+    <>
+      <div className="topsection girls-section">
+        <div className="container girls-container">
+          <div className="flex girls-flex">
+            <div className="girls-first-bar">
+              <div className="imageGallery">
+                <Carousel images={girlsData} />
+              </div>
+              <div className="girls-rightSide">
+                <div className="girls-description">
+                  BRHS Girls Volleyball are four time state champs as well as
+                  four time Somerset County Tournament Winners. The team has
+                  always been a dominant force and challengers to the
+                  championship. They had a strong performance last year, making
+                  it all the way to the semi-finals of the Somerset County
+                  Tournament before losing to Watchung Hills. Many players were
+                  recognized by the coaches across the conference and were given
+                  awards based on their stellar play.<br></br> <br></br>The team
+                  has a great relationship on and off the court, which speaks
+                  volumes to the environment built by the coaches and past
+                  players. A team bond is important to any sport and the one in
+                  BRHS Girls Volleyball Program is few and far between. There's
+                  a dynamic within the program that is a joy to be a part of and
+                  everyone genuinely enjoys their time there. <br></br>{" "}
+                  <br></br>With Coach Josh Everett at the helm, the team is an
+                  amazing thing to be a part of. Winning isn't everything, even
+                  though we do win a lot, the way we see it, every day is an
+                  opportunity to get better. Be it a win or a loss, a practice
+                  or a game, we take every day seriously and strive to grow as a
+                  team and people. We encourage all girls to tryout, regardless
+                  of their skill in volleyball! Tryouts are in August and be
+                  sure to join the Google Classroom for extra details.{" "}
+                </div>
+                <div className="girls-awards">
+                  <h1>TEAM AWARDS:</h1>
+                  Madison Patton: All Skyland Conference Delware Divison First
+                  Team <br></br>
+                  Isabel Rivera: All Skyland Conference Delware Divison First
+                  Team <br></br>
+                  Irene Papanastasiou: All Skyland Conference Delware Divison
+                  Second Team <br></br>
+                  Vasi Papanastasiou: All Skyland Conference Delware Divison
+                  Second Team <br></br>
+                  Izzy Antunes: All Skyland Conference Delware Divison Honorable
+                  Mention <br></br>
+                </div>
+                <div className="socials-girls">
+                  <div className="girls-insta">
+                    <a
+                      className="instagram"
+                      href="https://www.instagram.com/brhsgirlsvb/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <FontAwesomeIcon
+                        icon={faInstagram}
+                        color="white"
+                        className="icon-girls"
+                      />
+                    </a>
+                    <div className="girls-title">Girls Instagram</div>
+                  </div>
+                  <div className="hudl-girls">
+                    <div className="BRVBhudl">
+                      <a
+                        href="https://fan.hudl.com/usa/nj/bridgewater/organization/24379/bridgewater-raritan-high-school/team/324159/girls-varsity-volleyball"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {" "}
+                        <img
+                          className="girls-Hudl"
+                          src={hudl}
+                          alt="hudl"
+                        />{" "}
+                        {""}
+                      </a>
+                      <div className="girls-title">Girls Hudl</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="girls-players-tab">
+              <div className="girls-players-title">
+                <h1>THE 2024 GIRLS BRVB VARSITY TEAM</h1>
+              </div>
+              <div className="girls-roster">
+                <div className="girls-player">
+                  <img className="girls-player-img" src={liel} alt="" />
+                  Liel Raines Moshe
+                </div>
+                <div className="girls-player">
+                  <img className="girls-player-img" src={jada} alt="" />
+                  Jada Otero
+                </div>
+                <div className="girls-player">
+                  <img className="girls-player-img" src={liya} alt="" />
+                  Liya Medallion
+                </div>
+                <div className="girls-player">
+                  <img className="girls-player-img" src={blank} alt="" />
+                  Emily Evaristo
+                </div>
+                <div className="girls-player">
+                  <img className="girls-player-img" src={kylie} alt="" />
+                  Kylie Rosander
+                </div>
+                <div className="girls-player">
+                  <img className="girls-player-img" src={jahniah} alt="" />
+                  Jahniah Biship
+                </div>
+                <div className="girls-player">
+                  <img className="girls-player-img" src={irene} alt="" />
+                  Irene Papanastasiou
+                </div>
+                <div className="girls-player">
+                  <img className="girls-player-img" src={blank} alt="" />
+                  Madison Delia
+                </div>
+                <div className="girls-player">
+                  <img className="girls-player-img" src={hannah} alt="" />
+                  Hannah Arun
+                </div>
+                <div className="girls-player">
+                  <img className="girls-player-img" src={blank} alt="" />
+                  Brielle Diaz
+                </div>
+                <div className="girls-player">
+                  <img className="girls-player-img" src={blank} alt="" />
+                  Faye Gallou
+                </div>
+                <div className="girls-player">
+                  <img className="girls-player-img" src={blank} alt="" />
+                  Molly Magner
+                </div>
+                <div className="girls-player">
+                  <img className="girls-player-img" src={blank} alt="" />
+                  Ava Marvuglio
+                </div>
+
+                <div className="girls-player">
+                  <img className="girls-player-img" src={ella} alt="" />
+                  Ella Sorenson
+                </div>
+                <div className="girls-player">
+                  <img className="girls-player-img" src={blank} alt="" />
+                  Emily Leppert
+                </div>
+              </div>
+            </div>
+            <div id="girls-no-img">
+              Some images not found because they are new to the team. Please
+              wait for the season to go on for images to be taken of them.
+            </div>
+            <div className="girls-third-bar">
+              <div className="girls-coach">
+                <div className="coach-title">
+                  <h1>HEAD COACH JOSH EVERETT</h1>
+                </div>
+                <img src={josh} alt="josh" className="josh" />
+                <div className="coach-description">
+                  The coaching resume of Coach Josh Everett is very impressive.
+                  Before coming to BRHS as a coach, he coached girls and boys
+                  club for four seasons. He then transitioned to assistant
+                  varsity coach and junior varsity coach of both the girls and
+                  boys program. This past season, he was promoted to head coach
+                  of the girls varsity team. Before becoming a coach, Coach
+                  Everett was a player at BRHS and won a state championship. He
+                  also won another one as coach.
+                  <br></br>
+                  <br></br>
+                  Coach takes pride in making sure all of his players, not only
+                  respect him, but also each other. He makes great relationships
+                  with each of his players throughout practices and games. There
+                  will be many times when the team can just share a joke with
+                  him because of his charismatic personality. The environment he
+                  created is hard to come by and everyone enjoys being there
+                  with him as their leader.
+                </div>
+              </div>
+              <div className="girls-recent-news">
+                <div className="news-title">
+                  <h1>RECENT NJ GIRLS VOLLEYBALL NEWS</h1>
+                </div>
+                <div className="girls-news-item">
+                  <a
+                    href="https://www.nj.com/highschoolsports/2024/08/girls-volleyball-all-time-leaders-in-coaching-wins-entering-2024-season.html"
+                    target="blank"
+                    rel="noreferrer"
+                  >
+                    <img className="girls-news-img" src={girlsNews1} alt="" />
+                  </a>
+                  Girls volleyball: All-time leaders in coaching wins entering
+                  2024 season
+                </div>
+                <div className="girls-news-item">
+                  <a
+                    href="https://www.nj.com/highschoolsports/2024/08/compelling-girls-volleyball-storylines-to-follow-in-2024.html"
+                    target="blank"
+                    rel="noreferrer"
+                  >
+                    <img className="girls-news-img" src={girlsNews2} alt="" />
+                  </a>
+                  Compelling girls volleyball storylines to follow in 2024
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
